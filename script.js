d2c88862866f153ded621c109533db81c3d00849
Wolfie-eng <marina.dugardin.etu@univ-lille.fr>
2025-01-02 22:56:51 +0100

    3

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-/* Sélectionne le bouton */
-    const scrollToTopButton = document.querySelector('.scroll-to-top');
+// Mon code JavaScript
+const scrollToTopButton = document.querySelector('.scroll-to-top');
 
-    /* Affiche ou cache le bouton en fonction du défilement */
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 200) {
-            scrollToTopButton.classList.add('show');
-        } else {
-            scrollToTopButton.classList.remove('show');
-        }
-    });
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollToTopButton.classList.add('show');
+    } else {
+        scrollToTopButton.classList.remove('show');
+    }
+});
 
-    /* Ajoute l'événement de clic pour revenir en haut */
-    scrollToTopButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // Défilement fluide
-        });
+scrollToTopButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     });
+});
